9ad465ac205418a3a91c6512d21373e01cf30478
Denis <denis.c1@live.fr>
2025-04-14 09:26:51 +0000

    cv

--- src/app/Studies/studies.tsx
+++ src/app/Studies/studies.tsx
@@ -4,6 +4,73 @@ const Studies = () => {
     return (
         <div className={styles.main}>
             <h3> Formation </h3>
+            <div className={styles.row}>
+                <div className={styles.bold}>
+                    Ecole 42
+                </div>
+                <div>
+                    -
+                </div>
+                <div>
+                    Paris
+                </div>
+                <div>
+                    -
+                </div>
+                <div>
+                    2020 à 2023
+                </div>
+            </div>
+            <div className={styles.row}>
+                <div className={styles.bold}>
+                    Licence Informatique
+                </div>
+                <div>
+                    -
+                </div>
+                <div>
+                    UVSQ
+                </div>
+                <div>
+                    -
+                </div>
+                <div>
+                    2020
+                </div>
+            </div>
+            <div className={styles.row}>
+                <div className={styles.bold}>
+                    Etudes de de médecine & psychologie
+                </div>
+                <div>
+                    -
+                </div>
+                <div>
+                    Paris Descartes
+                </div>
+                <div>
+                    -
+                </div>
+                <div>
+                    2010 à 2014
+                </div>
+            </div>
+            <div className={styles.row}>
+                <div className={styles.bold}>
+                    Bac S spé Maths
+                </div>
+                <div>
+                    -
+                </div>
+                <div>
+                    Mention Européenne Anglais                </div>
+                <div>
+                    -
+                </div>
+                <div>
+                    2010
+                </div>
+            </div>
         </div>
     )
 }
